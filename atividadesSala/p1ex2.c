74a331034a205c7ac6ceabf0e040961ad3544f38
joao camarota <joao.camarota@discente.ufg.br>
2023-09-29 15:23:13 -0300

    copiei do gpt

--- atividadesSala/p1ex2.c
+++ atividadesSala/p1ex2.c
@@ -1,57 +1,50 @@
 #include <stdio.h>
 
+// Função para trocar dois elementos do vetor
+void trocar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Função de ordenação crescente (bubble sort)
+void ordenarCrescente(int vetor[], int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                trocar(&vetor[j], &vetor[j + 1]);
+            }
+        }
+    }
+}
+
+// Função de ordenação decrescente (bubble sort)
+void ordenarDecrescente(int vetor[], int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (vetor[j] < vetor[j + 1]) {
+                trocar(&vetor[j], &vetor[j + 1]);
+            }
+        }
+    }
+}
+
 int main() {
-    printf("jg");
-    int vetor[8] = {2,4,5,6,1,3,7,8};
-    int t = 1;
-    int aux;
+    int vetor[8] = {4, 2, 8, 7, 1, 5, 9, 3};
+    int meio = 4; // Metade do tamanho do vetor
 
-    for(int i = 0; i < 8; i++) {
+    // Ordena a primeira metade (valores pares) em ordem crescente
+    ordenarCrescente(vetor, meio);
 
-        if ((i /2) == 0) {
+    // Ordena a segunda metade (valores ímpares) em ordem decrescente
+    ordenarDecrescente(vetor + meio, meio);
 
-            while( t = 1) {
-                t = 0;
-
-                for(int j = 0; i < 4; i++) {
-
-                    if (vetor[j] > vetor[j+1]) {
-                        aux = vetor[j];
-                        vetor[j] = vetor[j+1];
-                        vetor[j+1] = vetor[j];
-
-                        if (t == 0) { t = 1; }
-                    }
-                    
-                }
-
-            }
-
-        }
-
-        else {
-
-            while( t = 1) {
-                t = 0;
-
-                for(int j = 3; i < 8; i++) {
-
-                    if (vetor[j] > vetor[j+1]) {
-                        aux = vetor[j];
-                        vetor[j] = vetor[j+1];
-                        vetor[j+1] = vetor[j];
-
-                        if (t == 0) { t = 1; }
-                    }
-                    
-                }
-
-            }
-        }
+    // Imprime o vetor reorganizado
+    printf("Vetor reorganizado:\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%d ", vetor[i]);
     }
+    printf("\n");
 
-    for (int k = 0; k < 8; k++) {
-        printf("%d, ", vetor[k]);
-    }
-
+    return 0;
 }
