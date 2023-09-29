e1f3c347d23d787c9766b24a73fd081c8f13cd2b
joao camarota <joao.camarota@discente.ufg.br>
2023-09-29 14:27:41 -0300

    nao sei o que estou fazendo

---
+++ atividadesSala/torreHanoi1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int base1[3] = {3,2,1};
+int base2[3] = {0,0,0};
+int base3[3] = {0,0,0};
+
+
+void main() {
+int origem, destino;
+printf("Base de origem:\n");
+scanf("%d" &origem);
+printf("%d", origem);
+return 0;
+
+}
+
