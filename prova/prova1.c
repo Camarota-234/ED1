8bce8d8d59d7b67a09c6a361ac97030e154c014d
camarota <joao.camarota@discente.ufg.br>
2023-11-01 17:22:41 -0300

    adicionando comentarios

--- prova/prova1.c
+++ prova/prova1.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-
+//Código pronto e funcional, diferentemente do original.
+//Áreas comentadas são trechos do código da maneira como foi entregue na prova.
 int main() {
+
     int aux = 0;
     int n = 8;
     int vetor[] = {1,3,5,2,4,6,7,8};
 
 
     for (int i = 0; i < n - 1 ; i++) {
-
-        for (int j = 0; j < n - i - 1; j++) {
+    //for (int i = 0; i <; i++) {
+        
+        for (int j = 0; j < n - i - 1; j++) { 
+        //for (int j = 0; j < n; j++) { 
 
             if (vetor[j] > vetor[j+1]) {
 
@@ -21,10 +25,13 @@ int main() {
     }
 
     for (int i = 0; i < n - 1 ; i++) {
+    //for (int i = 0; i <; i++) {
+        
+        for (int j = 0; j < n - i - 1; j++) { 
+        //for (int j = 0; j < n; j++) {
 
-        for (int j = 0; j < n - i - 1; j++) {
-
-            if ((vetor[j] % 2) != (j % 2)) {
+            if ((vetor[j] % 2) == (j % 2)) {
+            //if ((vetor[j] % 2) != (j % 2)) {
 
                 aux = vetor[j];
                 vetor[j] = vetor[j+1];
