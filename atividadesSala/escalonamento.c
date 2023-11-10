686f38a1c9c1b58cc7f17cd192b086bd27699ae4
João Guilherme <joao.camarota@discente.ufg.br>
2023-11-10 08:40:39 -0300

    nao esta funcionando

--- atividadesSala/escalonamento.c
+++ atividadesSala/escalonamento.c
@@ -2,26 +2,30 @@
 
 int main()
 {
-    int matriz[3][3];
-    int n = 5;
-    for (int i = 1; i < n; i++)
-    {
+    int matriz[2][3] = {{1,1,10} , {5,10,90}} ;
+    int n = 3;
+
+    printf(" %d %d %d \n %d %d %d \n\n ", matriz[0][0], matriz[0][1], matriz[0][2], matriz[1][0], matriz[1][1], matriz[1][2]);
+
+    for (int i = 1; i < n; i++) {
+
         int p = matriz[i][i];
 
-        for (int j = i; j < n + 1; j++)
-        {
-            matriz[i][j] = (1 / p) * matriz[j][j];
+        for (int j = i; j < n + 1; j++) {
+            matriz[i][j] = (1 / p) * matriz[i][j];
         }
 
-        for (int k = i + 1; k < n; k++)
-        {
-            int q = matriz[k][i];
+        for (int k = i + 1; k < n; k++){
 
-            for (int j = i; j < n + 1; j++)
-            {
+            int q = matriz[k][i]; 
 
+            for (int j = i; j < n + 1; j++) {
                 matriz[k][j] = matriz[k][j] - q * matriz[i][j];
             }
+
         }
     }
+
+    printf("%d %d %d \n %d %d %d ", matriz[0][0], matriz[0][1], matriz[0][2], matriz[1][0], matriz[1][1], matriz[1][2]);
+
 }
