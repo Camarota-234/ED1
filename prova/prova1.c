4714715157a9bf6830b7964e3a354e9a54b265bc
camarota <joao.camarota@discente.ufg.br>
2023-11-15 22:34:01 -0300

    percebi que meu codigo nao funciona

--- prova/prova1.c
+++ prova/prova1.c
@@ -6,7 +6,7 @@ int main() {
 
     int aux = 0;
     int n = 8;
-    int vetor[] = {2,3,5,1,4,6,7,8};
+    int vetor[] = {10,3,5,1,4,6,7,12};
 
 
     for (int i = 0; i < n - 1 ; i++) {
@@ -51,7 +51,7 @@ int main() {
 printf("\nVetor reorganizado:\n");
 
     for (int i = 0; i < n; i++) {
-        printf("%d", vetor[i]);
+        printf("%d ", vetor[i]);
     }
     
     return 0;
