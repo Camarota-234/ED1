8872b2484bf0bb82ce47dff65e2135f515f4297c
João Guilherme <joao.camarota@discente.ufg.br>
2023-12-08 07:57:49 -0300

    melhorando codigo

--- atividadesSala/filas.c
+++ atividadesSala/filas.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct no {
     int valor;
@@ -10,3 +10,42 @@ typedef struct fila {
     No *ultimo;
     int quantidade;
 } Fila;
+
+int primeiro(Fila *f) {
+    return f->primeiro->valor;
+}
+
+int quantidade_fila(Fila *f) {
+    return f->quantidade;
+}
+
+int vazia_fila(Fila *f) {
+    return (f->primeiro==NULL && f->ultimo==NULL);
+}
+
+void inicializar_fila(Fila *f){
+    f->primeiro = NULL;
+    f->ultimo = NULL;
+    f->quantidade = 0;
+
+}
+
+void enfilerar(Fila *f, int valor) {
+    No *n = malloc(sizeof(No));
+    n->valor = valor;
+    n->proximo = NULL;
+
+    if (f->ultimo == NULL) {
+        f->primeiro = n;
+    } else {
+        f->ultimo->proximo = n;
+    }
+    f->ultimo = n;
+    f->quantidade++;
+}
+
+
+int main() {
+    Fila f;
+    inicializar_fila(&f);
+}
